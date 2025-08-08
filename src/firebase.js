65f479787a5736ba81818edfc96f06e5903fdcb1
MatthewBarghout <mattbarg@unc.edu>
2025-08-08 17:30:21 -0400

    Implement secure environment variable handling
    - Add environment variables to GitHub Actions workflow
    - Revert firebase.js to use environment variables
    - Environment variables will be injected securely during CI/CD build

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFunctions } from "firebase/functions";
 import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBar49EHP9UicVfmSyKoBSDH0Mm3s7hh0o",
-  authDomain: "j-and-j-f8f66.firebaseapp.com",
-  projectId: "j-and-j-f8f66",
-  storageBucket: "j-and-j-f8f66.firebasestorage.app",
-  messagingSenderId: "1095564028251",
-  appId: "1:1095564028251:web:16391c61b9eac4f898d27e",
-  measurementId: "G-FP2YZ7W92Y"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
